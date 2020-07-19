d07848a5085efc13f4e260de7a6a98f63170bbd8
Krzysztof Wilczyński <kw@linux.com>
2020-07-19 23:33:26 +0200

    Refactor the capture_stderr helper to capture child processes output
    Signed-off-by: Krzysztof Wilczyński <kw@linux.com>

--- test/helpers/magic_test_helper.rb
+++ test/helpers/magic_test_helper.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
 module MagicTestHelpers
-  def capture_stderr
-    require 'stringio'
-    saved_stderr, $stderr = $stderr, StringIO.new
-    [yield, $stderr.string]
-  ensure
-    $stderr = saved_stderr
+  def capture_stderr(children: false)
+    require 'thread'
+    semaphore = Mutex.new
+    if children
+      require 'tempfile'
+      captured_stderr = Tempfile.new('captured_stderr')
+      semaphore.synchronize do
+        backup_stderr = $stderr.dup
+        $stderr.reopen captured_stderr
+        begin
+          yield
+          $stderr.rewind
+          captured_stderr.read
+        ensure
+          captured_stderr.unlink
+          $stderr.reopen backup_stderr
+        end
+      end
+    else
+      require 'stringio'
+      captured_stderr = StringIO.new
+      semaphore.synchronize do
+        backup_stderr = $stderr
+        $stderr = captured_stderr
+        begin
+          yield
+        ensure
+          $stderr = backup_stderr
+        end
+      end
+      captured_stderr.string
+    end
   end
 
   def with_fixtures(fixtures = 'fixtures', &block)
